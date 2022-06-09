075e1d170df45c224a3b4441ba07bf5534243e90
AlHassanHK <al.hassan.elkady@gmail.com>
2022-06-09 12:59:16 +0200

    add CORS

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 //! security routes middlewares
-const express = require('express');
-const productsRoute = require('./routes/productsRoutes')
+const express = require("express");
+const productsRoute = require("./routes/productsRoutes");
 const app = express();
 //! 1)security
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 //! 2) middlewares
 //? body paser, for reading data from the body
 app.use(express.json());
 //? to pass data coming from URL encoded form
-app.use(express.urlencoded({extended: true}));
-
+app.use(express.urlencoded({ extended: true }));
 
 //! 3) routes
-app.use('/api/v1/products',productsRoute)
+app.use("/api/v1/products", productsRoute);
 
-module.exports = app
+module.exports = app;
